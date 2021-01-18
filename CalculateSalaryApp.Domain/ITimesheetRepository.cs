7e62782915345b3d93ce7333812bad48e6dc6038
ArtemEvgl <Usbhawk5@gmail.com>
2021-01-18 21:58:19 +0300

    minor changes

---
+++ CalculateSalaryApp.Domain/ITimesheetRepository.cs
@@ -0,0 +1,17 @@
+﻿using CalculateSalaryApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateSalaryApp.Domain
+{
+    public interface ITimesheetRepository
+    {
+        void Add(TimeLog timeLog);
+        TimeLog[] GetTimeLogs(string lastName);
+
+
+    }
+}
